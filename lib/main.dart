e370fdeb82c94f24b2b073b88e3b0bb0b4677cc1
priyanka <bharadwajpriyanka204@gmail.com>
2023-02-21 08:32:58 -0800

    layout

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:layout5/home.dart';
+void main()
+{
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: home(),
+      )
+  );
+}
